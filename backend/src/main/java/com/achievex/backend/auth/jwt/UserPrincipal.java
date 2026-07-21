f532f2bbccef91c015c1b10db7f9e3ac7cfe1c5e
Mateus Dalagnol <dalagnolm@gmail.com>
2026-07-21 01:41:18 -0300

    feat: add JWT authentication filter and secure protected endpoints

---
+++ backend/src/main/java/com/achievex/backend/auth/jwt/UserPrincipal.java
@@ -0,0 +1,37 @@
+package com.achievex.backend.auth.jwt;
+
+import com.achievex.backend.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserPrincipal implements UserDetails {
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getId().toString();
+    }
+
+
+}
--- backend/src/main/java/com/achievex/backend/shared/controllers/HealthCheckController.java
+++ backend/src/main/java/com/achievex/backend/shared/controllers/HealthCheckController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @RequestMapping("/")
+    @RequestMapping("/check")
     public ResponseEntity<String> retornaJson() {
         return new ResponseEntity<>("{\"status\": \"ok\"}",HttpStatus.OK);
     }
